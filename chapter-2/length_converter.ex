d02afbccd3510b05001b2b780d4bbacd3e4b6938
Thiago Maceda <thiagopmaceda@gmail.com>
2022-06-04 22:59:01 -0300

    improve the chapter 2 length converter with defguard and general function to handle edge cases

--- chapter-2/length_converter.ex
+++ chapter-2/length_converter.ex
@@ -1,5 +1,15 @@
+defmodule NumberGuard do
+  defguard is_number_and_is_positive(value) when is_integer(value) and value >= 0
+end
+
 defmodule MeterToLengthConverter do
-  def convert(:feet, m) when is_number(m) and m >= 0, do: m * 3.28084
-  def convert(:inch, m) when is_number(m) and m >= 0, do: m * 39.3701
-  def convert(:yard, m) when is_number(m) and m >= 0, do: m * 1.09361
+  import NumberGuard
+
+  def convert(:feet, m) when is_number_and_is_positive(m), do: m * 3.28084
+  def convert(:inch, m) when is_number_and_is_positive(m), do: m * 39.3701
+  def convert(:yard, m) when is_number_and_is_positive(m), do: m * 1.09361
+
+  def convert(_, _) do
+    "you need to pass in the first argument the measure and in the second argument the number to convert"
+  end
 end
